0834193658036a8791ef0e3fee43747ee6145184
Erik Lucas Maros <eriklm42@gmail.com>
2024-04-13 19:25:10 -0300

    chore: add resource grafana

--- src/config/serverless.ts
+++ src/config/serverless.ts
@@ -1,24 +1,39 @@
-
 const baseServerlessConfig = {
-  provider: {
-    name: "aws",
-    runtime: "nodejs20.x",
-    stage: "dev",
-    region: "sa-east-1",
-    environment: {
-      VARIABLE_ONE: "value_one",
-      VARIABLE_TWO: "value_two",
-    },
-  },
-  plugins: ["serverless-offline"],
-  package: {
-    excludeDevDependencies: true,
-  },
-  custom: {
-    "serverless-offline": {
-      noPrependStageInUrl: true,
-    },
-  },
+	provider: {
+		name: "aws",
+		runtime: "nodejs20.x",
+		stage: "dev",
+		region: "sa-east-1",
+		environment: {
+			VARIABLE_ONE: "value_one",
+			VARIABLE_TWO: "value_two",
+		},
+	},
+	plugins: ["serverless-offline"],
+	package: {
+		excludeDevDependencies: true,
+	},
+	custom: {
+		"serverless-offline": {
+			noPrependStageInUrl: true,
+		},
+	},
+	resources: {
+		Resources: {
+			GrafanaDashboard: {
+				Type: "AWS::ApiGateway::Resource",
+				Properties: {
+					RestApiId: {
+						Ref: "ApiGatewayRestApi",
+					},
+					ParentId: {
+						"Fn::GetAtt": ["ApiGatewayRestApi", "RootResourceId"],
+					},
+					PathPart: "grafana",
+				},
+			},
+		},
+	},
 };
 
-export default baseServerlessConfig
+export default baseServerlessConfig;
